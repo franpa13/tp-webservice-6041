0c07c39196027735634c9c5debce5ffb95c7a64e
Francisco Javier Paredes <frandelregue@gmail.com>
2026-06-02 17:45:30 -0300

    add loader

--- src/app/pages/receta/receta.ts
+++ src/app/pages/receta/receta.ts
@@ -1,24 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { HlmButton } from '@spartan-ng/helm/button';
 import { HlmInput } from '@spartan-ng/helm/input';
 import { RecetaModel } from '../../models/receta.model';
 import { RecetasService } from '../../services/recetas.service';
 import { TranslateService } from '../../services/translate.service';
+import { HlmIcon } from "@spartan-ng/helm/icon";
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideCar, lucideLoader, lucideX } from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-receta',
-  imports: [FormsModule, HlmInput, HlmButton],
+  imports: [FormsModule, HlmInput, HlmButton, HlmIcon, NgIcon, HlmButton, HlmIcon],
+  providers: [provideIcons({ lucideCar, lucideX, lucideLoader })],
   templateUrl: './receta.html',
   styleUrl: './receta.css',
 })
 export class Receta {
   private readonly recetaService = inject(RecetasService);
   private readonly translateService = inject(TranslateService);
+
   receta: string = ""
   recetas = signal<RecetaModel[]>([])
   openModal = signal(false)
+  loading = signal(false)
   traducido = signal("")
   buscar() {
     this.recetaService.getRecetas(this.receta).subscribe({
@@ -32,17 +39,25 @@ export class Receta {
   }
 
   traducir(desc: string) {
+
     this.openModal.set(true)
+    this.loading.set(true)
     this.translateService.translate(desc).subscribe({
       next: (data: any) => {
-        return this.traducido.set(data.data.translations.translatedText)
+        this.traducido.set(data.data.translations.translatedText)
+        this.loading.set(false)
       },
       error: (err: any) => {
         console.log(err, "error");
+        this.loading.set(false)
       }
     })
   }
+
   closeModal() {
+ 
+    this.loading.set(false)
+    this.traducido.set("")
     this.openModal.set(false)
   }
 }
